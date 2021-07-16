a23274a9dcc65995a41fa94e7a841bddcd10b83e
eddy <hyun11262000@gmail.com>
2021-07-17 00:53:31 +0900

    Bringing smp data from DB

--- public/archive.js
+++ public/archive.js
@@ -6,17 +6,32 @@ const DB = require('../secure/DB_info.js');
 const db_connection = DB.info();
 
 const query_weather = `SELECT * FROM history_weather`;
+const query_smp = `SELECT * FROM history_smp`;
+
 
 router.get('/', function (req, res) {
     db_connection.query(query_weather, (err, results) => {
         if(err) throw err;
         else {
-            var weather_list = archive_template.weather_list(results);
-            var html = archive_template.HTML(weather_list);
+            var weather_list = archive_template.information_list(results);
+            var html = archive_template.HTML_weather(weather_list);
             res.write(html);
             res.end();
         }
     });
 });
+
+router.get('/smp', function (req, res) {
+    db_connection.query(query_smp, (err, results) => {
+        if(err) throw err;
+        else {
+            var smp_list = archive_template.information_list(results);
+            var html = archive_template.HTML_smp(smp_list);
+            res.write(html);
+            res.end();
+        }
+    });
+});
+
 
 module.exports = router;
--- public/view/archive_template.js
+++ public/view/archive_template.js
@@ -1,5 +1,5 @@
 module.exports = {
-    HTML:function(weather_list){
+    HTML_weather:function(information_list){
         return `
         <!DOCTYPE html>
         <html lang="EN">
@@ -16,15 +16,52 @@ module.exports = {
                 </button>
 
                 <h2>Weather Archive</h2>
+                <button disabled="disabled" class="button_selected" onClick="location.href='http://115.85.181.94:3000/archive/'">
+                    Weather
+                </button>
+                <button class="button" onClick="location.href='http://115.85.181.94:3000/archive/smp'">
+                    SMP
+                </button>
                 <hr color="#89b0ae" width="40%" size="4px" align="center">
                 <br>
-                ${weather_list}
+                ${information_list}
             </body>
         </html>
         `;   
     },
 
-    weather_list:function(data){
+    HTML_smp:function(information_list){
+        return `
+        <!DOCTYPE html>
+        <html lang="EN">
+            <head>
+                <title>Archive - smp</title>
+                <meta charset="utf-8">
+                <link type="text/css" rel="stylesheet" href="/css/main.css" />
+                <link type="text/css" rel="stylesheet" href="/css/button.css" />
+                <link type="text/css" rel="stylesheet" href="/css/table.css" />
+            </head>
+            <body>
+                <button class="button" onClick="location.href='http://115.85.181.94:3000/'">
+                    <img src="../image/home_icon.png" width="40px" height="40px" alt="Home">
+                </button>
+
+                <h2>SMP Archive</h2>
+                <button class="button" onClick="location.href='http://115.85.181.94:3000/archive/'">
+                    Weather
+                </button>
+                <button disabled="disabled" class="button_selected" onClick="location.href='http://115.85.181.94:3000/archive/smp'">
+                    SMP
+                </button>
+                <hr color="#89b0ae" width="40%" size="4px" align="center">
+                <br>
+                ${information_list}
+            </body>
+        </html>
+        `;   
+    },
+
+    information_list:function(data){
         var list= '<table class="user-list" border="0" align="center">';
         list += '<th>Date</th>';
 
@@ -47,4 +84,5 @@ module.exports = {
         list += '</table><br><br>';
         return list;
     }
+
 }
