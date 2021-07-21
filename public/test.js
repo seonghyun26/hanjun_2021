4982e16ac4da871aa855f228b982f51e75dcc12d
eddy <hyun11262000@gmail.com>
2021-07-21 22:39:03 +0900

    Arduino Communication Done

---
+++ public/test.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const request = require('request');
+const router = express.Router();
+
+const URL = 'http://hyungu.asuscomm.com/';
+
+const test_template = require('./view/test_template.js');
+
+router.get('/', function (req, res) {
+    var html = test_template.HTML();
+    res.write(html);
+    res.end();
+});
+
+router.get('/:number/:on_off', function (req, res) {
+    const query_number = req.params.number;
+    const query_on_off = req.params.on_off;
+    const params = encodeURIComponent(query_number) + '/' + encodeURIComponent(query_on_off) + '/';
+    request({
+        url: URL + params
+    }, function (error, response, body) {
+        if (error) throw error;
+        else {
+            // console.log("type of response : " + typeof(response));
+            var pasrsedJSON = response.body;
+            console.log(pasrsedJSON);
+            res.redirect('/test');
+        }
+    });
+    
+});
+
+module.exports = router;
---
+++ public/view/test_template.js
@@ -0,0 +1,49 @@
+module.exports = {
+  HTML:function(){
+    return `
+    <!DOCTYPE html>
+    <html lang="EN">
+      <head>
+        <title>Test</title>
+        <meta charset="utf-8">
+        <link type="text/css" rel="stylesheet" href="/css/main.css" />
+        <link type="text/css" rel="stylesheet" href="/css/button.css" />
+        <link type="text/css" rel="stylesheet" href="/css/input.css" />
+      </head>
+      <body>
+        <button class="button" onClick="location.href='http://115.85.181.94:3000/'">
+          <img src="http://115.85.181.94:3000/image/home_icon.png" width="40px" height="40px" alt="Home">
+        </button>
+    
+        <h2> Arudino Test Page </h2>
+
+        <button class="button" onClick="location.href='/test/A/0'">
+          A OFF
+        </button>
+        <button class="button" onClick="location.href='/test/A/1'">
+          A ON
+        </button>
+        <br><br>
+
+        <button class="button" onClick="location.href='/test/B/0'">
+          B OFF
+        </button>
+        <button class="button" onClick="location.href='/test/B/1'">
+          B ON
+        </button>
+        <br><br>
+
+        <button class="button" onClick="location.href='/test/C/0'">
+          C OFF
+        </button>
+        <button class="button" onClick="location.href='/test/C/1'">
+          C ON
+        </button>
+    
+    
+    
+      </body>
+    </html>
+    `
+  }   
+}
