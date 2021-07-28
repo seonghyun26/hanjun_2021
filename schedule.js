8a0d57d013d3b4d28d82217328d97cca6e38d198
eddy <hyun11262000@gmail.com>
2021-07-28 09:10:02 +0900

    Testing every hour

--- schedule.js
+++ schedule.js
@@ -1,16 +1,26 @@
-var schedule = require('node-schedule');
-var rule = new schedule.RecurrenceRule();
+const schedule = require('node-schedule');
 
 const DB = require('./secure/DB_info');
 const db_connection = DB.info();
 
-/*
-    * : every value
-    /n : do every n
-*/
-// second, minute, hour, day of month, month, dae of week
-// to stop, test.cancel();
-var test = schedule.scheduleJob('*/10 * * * * *', function(){ 
+const QUERY_TEST = function (par1, par2) {
+    return `
+        INSERT INTO schedule_test (test_a, test_b) VALUES
+        (
+            ${par1}, ${par2}
+        )
+    `
+};
+
+
+// ** How to use **
+// - * : every value
+// - /n : do every n
+// 6 values : second, minute, hour, day of month, month, dae of week
+// To stop : test.cancel();
+
+
+const test = schedule.scheduleJob('*/10 * * * * *', function(){ 
     const dt = new Date();
     const min = dt.getMinutes();
     const sec = dt.getSeconds();
@@ -28,18 +38,14 @@ var test = schedule.scheduleJob('*/10 * * * * *', function(){
 test.cancel();
 
 
-var test_2 = schedule.scheduleJob('0 0 */1 * * *', function(){ 
+const test_2 = schedule.scheduleJob('0 0 */1 * * *', function(){ 
     const dt = new Date();
     const hour = dt.getHours();
     const min = dt.getMinutes();
     // console.log(`current time : ${dt}\n`);
     db_connection.query(
-        `
-            INSERT INTO schedule_test (test_a, test_b) VALUES
-            (
-                ${hour}, ${min}
-            )
-        `, (err, results) => {
+        QUERY_TEST(hour, min), (err, results) => {
+            if(err) throw err;
         }
     )
 });
